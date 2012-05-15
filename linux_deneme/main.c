0bbff5567d93f2a10b5ec1398600ced1a4f4932d
ycansener <ycansener@gmail.com>
2012-05-15 18:53:52 +0300

    Proje dosyası oluşturuldu. Kaynak kod geliştirildi.

---
+++ linux_deneme/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Bu program;\n\n");
+    printf("Ege Bilmuh GNU Linux\nve daha dilimizin donmedigi pek çok sifati barindiran toplulugun; \n\n");
+    printf("Yigitcan SENER; \n");
+    printf("                                \n\nadli penguenlerinin, \n\n\nyazilim dunyasina ve github ailesine bir armaganidir. \n");
+
+
+    printf("     .--.  \n");
+    printf("    |o_o |  \n");
+    printf("    |:_/ |  \n");
+    printf("   //   \ \  \n");
+    printf("  (|     | )  \n");
+    printf(" /'|_   _/'\  \n");
+    printf(" \___)=(___/  \n");
+    return 0;
+}
